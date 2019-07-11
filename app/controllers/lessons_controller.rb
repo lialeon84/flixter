03c53e1ec66b3a652863c5ebe969521c784478a0
Rosa <lealeon@att.net>
2019-07-11 22:15:32 +0000

    added credit card pmts

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,29 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
   def show
   end
-   private
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def enroll
+    @lesson = current_user.enroll_in(lesson_params)
+    redirect_to instructor_course_path(current_lesson.course)
+ end
+
+ private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.course.user != current_user
+      return render plain: 'Must enroll', status: :unenrolled
+    end
+  end
+
+
+   
 
   helper_method :current_lesson
   def current_lesson
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,11 +4,9 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
+  
     storage :aws
-  else
-    storage :file
-  end
+  
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
